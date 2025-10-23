f674b553186570afae4534f157245024e6024b38
Wouter van der Velde <woutervandervelde2000@hotmail.com>
2025-10-23 22:54:42 +0200

    Improved animation style and order, still issue with recomposition

--- app/src/main/java/com/example/composepractice/ui/project/counter/Counter.kt
+++ app/src/main/java/com/example/composepractice/ui/project/counter/Counter.kt
@@ -1,16 +1,20 @@
 package com.example.composepractice.ui.project.counter
 
+import android.util.Log
 import androidx.compose.animation.core.Animatable
 import androidx.compose.animation.core.EaseInOut
 import androidx.compose.animation.core.tween
+import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
@@ -21,12 +25,18 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ImageBitmap
 import androidx.compose.ui.graphics.TransformOrigin
 import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.res.imageResource
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.drawText
+import androidx.compose.ui.text.rememberTextMeasurer
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.IntSize
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.composepractice.R
@@ -35,28 +45,71 @@ import com.example.composepractice.R
 fun Counter(
     day: Int,
     modifier: Modifier = Modifier,
+    start: Int = 21,
+    end: Int = 8,
+    textStyle: TextStyle = MaterialTheme.typography.headlineMedium.copy(fontSize = 48.sp),
 ) {
-    val offset = 9 - day
-    var currentNumber by remember { mutableIntStateOf(day + offset) }
+    val tens = mutableListOf<Int>()
+    val ones = mutableListOf<Int>()
+    for (n in start downTo end) {
+        tens += n / 10
+        ones += n % 10
+    }
+
+    Row(modifier = modifier) {
+        SingleCounter(
+            animationList = tens
+        )
+        SingleCounter(
+            animationList = ones
+        )
+    }
+}
+
+@Composable
+private fun SingleCounter(
+    animationList: List<Int>,
+    modifier: Modifier = Modifier
+) {
+    val animationDuration = 5000
+    val flipDelay = 1000f
+    val flipDuration = 500f
+    val animation = remember { Animatable(0f) }
+
+    LaunchedEffect(Unit) {
+        animation.animateTo(
+            targetValue = animationList.size * flipDelay + (flipDuration - flipDelay),
+            animationSpec = tween(durationMillis = animationDuration)
+        )
+    }
 
     Box(
         contentAlignment = Alignment.TopCenter,
         modifier = modifier
-            .shadow(
-                elevation = 8.dp,
-                shape = RoundedCornerShape(8.dp),
-                clip = false
-            )
     ) {
-        CountdownCard(day = currentNumber)
+        CountdownCard(day = animationList.first())
         CountdownCardRing(modifier.graphicsLayer { translationY = -8.dp.toPx() })
-        CountdownCardAnimation(
-            day = day,
-            offset = offset,
-            setCurrentNumber = { currentNumber = it }
-        )
-    }
 
+        animationList.forEachIndexed { index, number ->
+            val startFrom = index * flipDelay
+            val endAt = startFrom + flipDuration
+
+            val timeElapsed = (animation.value - startFrom)
+                .coerceAtLeast(0f)
+                .coerceAtMost(flipDuration)
+
+            val progress = timeElapsed / flipDuration
+            val rotationX = (1f - progress) * 270f
+
+            CountdownCard(
+                day = number,
+                modifier = Modifier.graphicsLayer {
+                    this.transformOrigin = TransformOrigin(0.5f, 0f)
+                    this.rotationX = rotationX
+                }
+            )
+        }
+    }
 }
 
 @Composable
@@ -87,43 +140,6 @@ private fun CountdownCardRing(modifier: Modifier = Modifier) {
         contentDescription = null,
         modifier = modifier.width(4.dp)
     )
-}
-
-@Composable
-private fun CountdownCardAnimation(
-    day: Int,
-    offset: Int,
-    durationMs: Int = 750,
-    setCurrentNumber: (Int) -> Unit
-) {
-    for (i in (day + offset - 1) downTo day) {
-        val index = (day + offset - 1) - i
-        var visible by remember { mutableStateOf(true) }
-        val rotation = remember { Animatable(270f) }
-
-        val t = index.toFloat() / (offset - 1)
-        val scale = EaseInOut.transform(t)
-        val delayMs = (scale * durationMs).toInt()
-
-        if (visible) {
-            CountdownCard(
-                day = i,
-                modifier = Modifier.graphicsLayer {
-                    transformOrigin = TransformOrigin(0.5f, 0f)
-                    rotationX = rotation.value
-                }
-            )
-
-            LaunchedEffect(Unit) {
-                rotation.animateTo(
-                    targetValue = 0f,
-                    animationSpec = tween(durationMillis = 1000, delayMillis = delayMs)
-                )
-                setCurrentNumber(i)
-                visible = false
-            }
-        }
-    }
 }
 
 @Preview
